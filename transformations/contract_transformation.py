dbcdc3ca70c5b493bd8d25104b839f2f2c2307f3
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-24 17:55:53 +0700

    add link client contract hashkey

--- transformations/contract_transformation.py
+++ transformations/contract_transformation.py
@@ -1,6 +1,17 @@
 import hashlib
 
+def get_hashkey(key):
+    return hashlib.md5(key.encode('utf-8'))
+
+def hashkey_to_bytes_array(hashkey):
+    return list(hashkey.digest())
+
 def transform(entity, msg_context):
-    key = (str(entity['id']) + '|' + str(msg_context['source'])).encode('utf-8')
-    entity['hashKey'] = list(hashlib.md5(key).digest())
+    contract_key = str(entity['id']) + '|' + str(msg_context['source'])
+    contract_hashkey = get_hashkey(contract_key)
+    entity['hashKey'] = hashkey_to_bytes_array(contract_hashkey)
+
+    link_client_contract_key = str(contract_hashkey.hexdigest()) + '|' + '|' + str(msg_context['source'])
+    link_client_contract_hashkey = get_hashkey(link_client_contract_key)
+    entity['linkClientContractHashkey'] = hashkey_to_bytes_array(link_client_contract_hashkey)
     return entity
