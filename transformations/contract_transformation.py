5a3d4005801d62334f5be5d115fdff9ce8a98022
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-24 11:58:24 +0700

    fix hashKey attribute name

--- transformations/contract_transformation.py
+++ transformations/contract_transformation.py
@@ -2,5 +2,5 @@ import hashlib
 
 def transform(entity, msg_context):
     key = (str(entity['id']) + '|' + str(msg_context['source'])).encode('utf-8')
-    entity['hashCode'] = hashlib.md5(key).hexdigest()
+    entity['hashKey'] = hashlib.md5(key).hexdigest()
     return entity
