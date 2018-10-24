773fe7ae8cbe3a9c4d465c0c1d6b72f14fcf35bd
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-24 12:18:53 +0700

    testing - hashKey will be a list

--- transformations/contract_transformation.py
+++ transformations/contract_transformation.py
@@ -2,5 +2,5 @@ import hashlib
 
 def transform(entity, msg_context):
     key = (str(entity['id']) + '|' + str(msg_context['source'])).encode('utf-8')
-    entity['hashKey'] = hashlib.md5(key).hexdigest()
+    entity['hashKey'] = list(hashlib.md5(key).digest())
     return entity
