913f6be501cf42e6ec5d4371ac727af0b0352abf
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-24 07:54:11 +0700

    cast to string before concat

--- transformations/contract_transformation.py
+++ transformations/contract_transformation.py
@@ -1,6 +1,6 @@
 import hashlib
 
 def transform(entity, msg_context):
-    key = (entity['id'] + '|' + msg_context['source']).encode('utf-8')
+    key = (str(entity['id']) + '|' + str(msg_context['source'])).encode('utf-8')
     entity['hashCode'] = hashlib.md5(key).hexdigest()
     return entity
