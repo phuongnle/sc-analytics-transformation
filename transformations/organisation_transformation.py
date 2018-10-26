ced2e63598869dadf14d63a70e1741179b931803
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-26 11:14:56 +0700

    calculate organisationRowHash & versionRowHash

--- transformations/organisation_transformation.py
+++ transformations/organisation_transformation.py
@@ -13,6 +13,21 @@ def transform(entity, msg_context):
 
     version_key = str(organisation_hashkey) + '|' + str(entity['version'])
     version_hash = get_hash(version_key)
-    entity['rowHash'] = hash_object_to_bytes_array(version_hash)
+    entity['versionRowHash'] = hash_object_to_bytes_array(version_hash)
+
+    organisation_row_key = \
+        str(organisation_hashkey) + '|' + str(msg_context['source']) + '|' + \
+        str(msg_context['id']) + '|' + str(msg_context['item url']) + '|' + str(msg_context['registered name']) + '|' + \
+        str(msg_context['retired']) + '|' + str(msg_context['deleted']) + '|' + str(msg_context['previous registered name']) + '|' + \
+        str(msg_context['trading name']) + '|' + str(msg_context['previous trading name']) + '|' + str(msg_context['organisation number']) + '|' + \
+        str(msg_context['business number']) + '|' + str(msg_context['business number registration date']) + '|' + str(msg_context['company number']) + '|' + \
+        str(msg_context['date of incorporation']) + '|' + str(msg_context['registered for gst']) + '|' + str(msg_context['indigenous owned entity']) + '|' + \
+        str(msg_context['disability employment organisation']) + '|' + str(msg_context['erp id']) + '|' + str(msg_context['organisation website']) + '|' + \
+        str(msg_context['organisation email']) + '|' + str(msg_context['organisation fax']) + '|' + str(msg_context['business address line 1']) + '|' + \
+        str(msg_context['business address line 2']) + '|' + str(msg_context['business address line 3']) + '|' + str(msg_context['business suburb']) + '|' + \
+        str(msg_context['business postcode']) + '|' + str(msg_context['postal address line 1']) + '|' + str(msg_context['postal address line 2']) + '|' + \
+        str(msg_context['postal address line 3']) + '|' + str(msg_context['postal suburb']) + '|' + str(msg_context['postal postcode']) + '|' + \
+        str(msg_context['notes']) + '|' + str(msg_context['phone number']) + '|' + str(msg_context['organisation type'])
+    entity['organisationRowHash'] = hash_object_to_bytes_array(get_hash(organisation_row_key))
 
     return entity
