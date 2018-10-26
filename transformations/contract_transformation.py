e12dab610e52af3e15672beaab3fc7707d65c0e8
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-26 14:43:47 +0700

    search for supplier hash key

--- transformations/contract_transformation.py
+++ transformations/contract_transformation.py
@@ -1,4 +1,5 @@
 import hashlib
+import binascii
 
 def get_hash(key):
     return hashlib.md5(key.encode('utf-8'))
@@ -6,21 +7,35 @@ def get_hash(key):
 def hash_object_to_bytes_array(hashkey):
     return list(hashkey.digest())
 
-def transform(entity, msg_context):
+def get_organisation_hashKey(organisation_id, sql_helper):
+    if organisation_id is None:
+        return None
+    query = "select top 1 [Organisation HashKey] from [Data Vault].[Hub Organisation] where [Organisation ID] = ?"
+    return sql_helper.fetch_value(query, organisation_id)
+
+def transform(entity, msg_context, sql_helper):
     contract_key = str(entity['id']) + '|' + str(msg_context['source'])
     contract_hashkey = get_hash(contract_key)
     entity['contractHashkey'] = hash_object_to_bytes_array(contract_hashkey)
+
+    version_key = str(contract_hashkey) + '|' + str(entity['version'])
+    version_hash = get_hash(version_key)
+    entity['rowHash'] = hash_object_to_bytes_array(version_hash)
+
+    organisation_hashKey = get_organisation_hashKey(entity['organisation id'], sql_helper)
+    if organisation_hashKey is not None:
+        entity['supplierHashKey'] = list(organisation_hashKey)
+        link_supplier_contract_key = str(contract_hashkey.hexdigest()) + '|' + binascii.hexlify(organisation_hashKey).decode('utf-8') + '|' + str(msg_context['source'])
+    else:
+        link_supplier_contract_key = str(contract_hashkey.hexdigest()) + '|' + '|' + str(msg_context['source'])
+
+    link_supplier_contract_hashkey = get_hash(link_supplier_contract_key)
+    entity['linkSupplierContractHashkey'] = hash_object_to_bytes_array(link_supplier_contract_hashkey)
+
 
     link_client_contract_key = str(contract_hashkey.hexdigest()) + '|' + '|' + str(msg_context['source'])
     link_client_contract_hashkey = get_hash(link_client_contract_key)
     entity['linkClientContractHashkey'] = hash_object_to_bytes_array(link_client_contract_hashkey)
 
-    link_supplier_contract_key = str(contract_hashkey.hexdigest()) + '|' + '|' + str(msg_context['source'])
-    link_supplier_contract_hashkey = get_hash(link_supplier_contract_key)
-    entity['linkSupplierContractHashkey'] = hash_object_to_bytes_array(link_supplier_contract_hashkey)
-
-    version_key = str(contract_hashkey) + '|' + str(entity['version'])
-    version_hash = get_hash(version_key)
-    entity['rowHash'] = hash_object_to_bytes_array(version_hash)
 
     return entity
