60ea5e82d446c8767c4dbdf953a67470acb4e3df
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-29 07:03:10 +0700

    add contract_segment

---
+++ transformations/contract_segment_transformation.py
@@ -0,0 +1,19 @@
+import hashlib
+import binascii
+
+def get_hash(key):
+    return hashlib.md5(key.encode('utf-8'))
+
+def hash_object_to_bytes_array(hashkey):
+    return list(hashkey.digest())
+
+def transform(entity, msg_context, sql_helper):
+    contract_segment_key = str(entity['id']) + '|' + str(msg_context['source'])
+    contract_hashkey = get_hash(contract_segment_key)
+    entity['contractSegmentHashkey'] = hash_object_to_bytes_array(contract_segment_key)
+
+    version_key = str(contract_hashkey) + '|' + str(entity['version'])
+    version_hash = get_hash(version_key)
+    entity['rowHash'] = hash_object_to_bytes_array(version_hash)
+
+    return entity
