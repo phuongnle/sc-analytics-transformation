a2fca91b6cbdcebe4d236c05cb9ea87e4ce4b9bc
Phuong N. Le <phuongnamle.vn@gmail.com>
2018-10-29 07:15:26 +0700

    fix contract segment transformation

--- transformations/contract_segment_transformation.py
+++ transformations/contract_segment_transformation.py
@@ -9,10 +9,10 @@ def hash_object_to_bytes_array(hashkey):
 
 def transform(entity, msg_context, sql_helper):
     contract_segment_key = str(entity['id']) + '|' + str(msg_context['source'])
-    contract_hashkey = get_hash(contract_segment_key)
-    entity['contractSegmentHashkey'] = hash_object_to_bytes_array(contract_segment_key)
+    contract_segment_hashkey = get_hash(contract_segment_key)
+    entity['contractSegmentHashkey'] = hash_object_to_bytes_array(contract_segment_hashkey)
 
-    version_key = str(contract_hashkey) + '|' + str(entity['version'])
+    version_key = str(contract_segment_key) + '|' + str(entity['version'])
     version_hash = get_hash(version_key)
     entity['rowHash'] = hash_object_to_bytes_array(version_hash)
 
