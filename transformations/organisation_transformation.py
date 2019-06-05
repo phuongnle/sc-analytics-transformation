318e68ee91b414d41a328ea134f59d6ca2becf3f
Phuong Le <phuongnamle.vn@gmail.com>
2019-06-05 10:29:55 +0700

    Update organisation_transformation.py

--- transformations/organisation_transformation.py
+++ transformations/organisation_transformation.py
@@ -1,5 +1,5 @@
 import hashlib
 
 def transform(entity, msg_context):
-    entity['Organisation Number'] = str(entity['Organisation Number']) + " edited"
+    entity['Previous Registered Name'] = str(entity['Previous Registered Name']) + " edited"
     return entity
